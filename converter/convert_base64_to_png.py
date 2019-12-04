77ab2a14bb845754d7861a46f5a101536c1b0853
abby <abby@vradars.com>
2019-12-04 23:11:15 +0530

    Added convertor and sample sensor data

--- converter/convert_base64_to_png.py
+++ converter/convert_base64_to_png.py
@@ -2,7 +2,7 @@ import base64
 import json
 from PIL import Image
 from io import BytesIO
-
+import sys
 
 filename = sys.argv[1]
 
@@ -11,6 +11,6 @@ with open(filename, "r") as response_file:
 
 index = 0
 for image in response_data['images']:
-    index++
-    with open("simulation_00" + index, "wb") as im:
-        im.write(base64.decodebytes(image))
+    index = index + 1
+    im = Image.open(BytesIO(base64.b64decode(image)))
+    im.save('simulation_' + str(index) + '.png', 'PNG')
